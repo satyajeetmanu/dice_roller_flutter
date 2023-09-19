6684bfcffb73f3f320e5c6fd796ade0537694883
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2023-09-20 00:18:00 +0530

    built a basic screen which says hello world!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('Hello World!'),
-  ));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 51, 1, 101),
+                Color.fromARGB(255, 77, 2, 153),
+                Color.fromARGB(255, 108, 1, 196),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(fontSize: 28, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
